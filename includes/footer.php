49945d8d7b72a3ac0c064837400d75af234d8641
quyencao <boy20195@gmail.com>
2017-03-05 00:10:39 +0700

    Take out header, footer, navigation from index.php

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+
+<!-- Footer -->
+<footer class="py-5 bg-inverse">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+    </div>
+    <!-- /.container -->
+</footer>
+
+<!-- jQuery Version 3.1.1 -->
+<script src="js/jquery.js"></script>
+
+<!-- Tether -->
+<script src="js/tether.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Blog Home - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/blog-home.css" rel="stylesheet">
+
+    <!-- Temporary fix for navbar responsiveness -->
+    <style>
+    .navbar-toggler {
+    z-index: 1;
+    }
+
+    @media (max-width: 576px) {
+    nav > .container {
+        width: 100%;
+    }
+    }
+    </style>
+
+</head>
+
+<body>
