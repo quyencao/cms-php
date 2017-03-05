297a0bb8bfaf97dd08b5b64020e2d3d413762da3
quyencao <boy20195@gmail.com>
2017-03-05 22:21:43 +0700

    Display post data

--- admin/posts.php
+++ admin/posts.php
@@ -32,17 +32,40 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>3</td>
-                            <td>4</td>
-                            <td>5</td>
-                            <td>6</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>9</td>
-                        </tr>
+
+                        <?php
+
+                            $query = "SELECT * FROM posts";
+
+                            $select_posts = mysqli_query($connection, $query);
+
+                            while ($row = mysqli_fetch_assoc($select_posts)) {
+
+                                $post_id = $row["post_id"];
+                                $post_author = $row["post_author"];
+                                $post_title = $row["post_title"];
+                                $post_category_id = $row["post_category_id"];
+                                $post_status = $row["post_status"];
+                                $post_image = $row["post_image"];
+                                $post_tags = $row["post_tags"];
+                                $post_comment_count = $row["post_comment_count"];
+                                $post_date = $row["post_date"];
+
+                                echo "<tr>";
+                                echo "<td>{$post_id}</td>";
+                                echo "<td>{$post_author}</td>";
+                                echo "<td>{$post_title}</td>";
+                                echo "<td>{$post_category_id}</td>";
+                                echo "<td>{$post_status}</td>";
+                                echo "<td><img src='images/{$post_image}' class='img-responsive' width='100px'/></td>";
+                                echo "<td>{$post_tags}</td>";
+                                echo "<td>{$post_comment_count}</td>";
+                                echo "<td>{$post_date}</td>";
+                                echo "</tr>";
+                            }
+
+                        ?>
+
                     </tbody>
                 </table>
             </div>
