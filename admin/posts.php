3f2c2dd488cd0455d22cdff1ee017c75f2abb81a
quyencao <boy20195@gmail.com>
2017-03-05 23:18:07 +0700

    Use switch statment to include diffirent content in posts

--- admin/posts.php
+++ admin/posts.php
@@ -11,63 +11,38 @@
 
             <!-- Page Heading -->
             <div class="row">
+
                 <div class="col-lg-12">
                     <h1 class="page-header">
                         Welcome to admin
                         <small>Quyen</small>
                     </h1>
                 </div>
-                <table class="table table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Author</th>
-                            <th>Title</th>
-                            <th>Category</th>
-                            <th>Status</th>
-                            <th>Image</th>
-                            <th>Tags</th>
-                            <th>Comments</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
 
-                        <?php
+                <?php
 
-                            $query = "SELECT * FROM posts";
+                    if(isset($_GET["source"])) {
 
-                            $select_posts = mysqli_query($connection, $query);
+                        $source = $_GET["source"];
 
-                            while ($row = mysqli_fetch_assoc($select_posts)) {
+                    } else {
 
-                                $post_id = $row["post_id"];
-                                $post_author = $row["post_author"];
-                                $post_title = $row["post_title"];
-                                $post_category_id = $row["post_category_id"];
-                                $post_status = $row["post_status"];
-                                $post_image = $row["post_image"];
-                                $post_tags = $row["post_tags"];
-                                $post_comment_count = $row["post_comment_count"];
-                                $post_date = $row["post_date"];
+                        $source = "";
 
-                                echo "<tr>";
-                                echo "<td>{$post_id}</td>";
-                                echo "<td>{$post_author}</td>";
-                                echo "<td>{$post_title}</td>";
-                                echo "<td>{$post_category_id}</td>";
-                                echo "<td>{$post_status}</td>";
-                                echo "<td><img src='images/{$post_image}' class='img-responsive' width='100px'/></td>";
-                                echo "<td>{$post_tags}</td>";
-                                echo "<td>{$post_comment_count}</td>";
-                                echo "<td>{$post_date}</td>";
-                                echo "</tr>";
-                            }
+                    }
 
-                        ?>
+                    switch ($source) {
 
-                    </tbody>
-                </table>
+                        case "a":
+                            echo "A";
+                            break;
+                        default:
+                            include "includes/view_all_posts.php";
+                            break;
+                    }
+
+                ?>
+
             </div>
         <!-- /.container-fluid -->
 
