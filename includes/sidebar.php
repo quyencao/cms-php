4bf799d9b712c43c241111847a262666b6ccf05b
quyencao <boy20195@gmail.com>
2017-03-05 10:59:14 +0700

    Search engine part 2 Make a query to database

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -5,7 +5,21 @@
         if(isset($_POST["submit"])) {
             $search = $_POST["search"];
 
-            echo $search;
+            $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+
+            $search_query = mysqli_query($connection, $query);
+
+            if(!$search_query) {
+                die ("QUERY FAILED " . mysqli_error($connection));
+            }
+
+            $count = mysqli_num_rows($search_query);
+
+            if($count == 0) {
+                echo "<h1>NO RESULT</h1>";
+            } else {
+                echo $count;
+            }
         }
 
     ?>
