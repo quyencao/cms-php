eadf8f1e9ae637cf737055e07675c04aa4cb64fa
quyencao <boy20195@gmail.com>
2017-03-05 15:52:07 +0700

    Clean up admin navigation

--- admin/index.php
+++ admin/index.php
@@ -16,14 +16,6 @@
                             Welcome to admin
                             <small>Quyen</small>
                         </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
                     </div>
                 </div>
                 <!-- /.row -->
