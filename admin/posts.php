f3220f12be8cfda63c558352890a3a97b0f38090
quyencao <boy20195@gmail.com>
2017-03-05 21:59:04 +0700

    Add posts table

---
+++ admin/posts.php
@@ -0,0 +1,57 @@
+<?php include "includes/admin_header.php"; ?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"; ?>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Welcome to admin
+                        <small>Quyen</small>
+                    </h1>
+                </div>
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Author</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Image</th>
+                            <th>Tags</th>
+                            <th>Comments</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                            <td>5</td>
+                            <td>6</td>
+                            <td>7</td>
+                            <td>8</td>
+                            <td>9</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        <!-- /.container-fluid -->
+
+        </div>
+    <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <?php include "includes/admin_footer.php"; ?>
