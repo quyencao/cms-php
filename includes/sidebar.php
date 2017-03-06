c9893ec0e2e177f7a8a18b9c63e31208d693d0ac
quyencao <boy20195@gmail.com>
2017-03-06 23:31:38 +0700

    Filter post by category

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -36,8 +36,9 @@
                         <?php
                             while($row = mysqli_fetch_assoc($select_categories_sidebar)) {
 
+                                $cat_id = $row["cat_id"];
                                 $cat_title = $row["cat_title"];
-                                echo "<li><a href='#' class='nav-link'>{$cat_title}</a></li>";
+                                echo "<li><a href='category.php?cat_id={$cat_id}' class='nav-link'>{$cat_title}</a></li>";
 
                             }
                         ?>
